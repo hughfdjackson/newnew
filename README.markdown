5c3c883c3da5958d26f4c7d3b5d240ee91eb31d7
hugh fd jackson <hughfdjackson@googlemail.com>
2012-10-30 05:08:21 -0400

    done with tests for the night; getting the readme together

---
+++ README.markdown
@@ -0,0 +1,26 @@
+## newnew
+
+`newnew` is the new `new`. 
+
+### What? 
+
+`newnew` is a function that replaces the `new` operator.
+
+### WHy??
+
+`newnew` inherits the first-class nature of functions, allowing you to do a couple of cool things that you never could with `new`: 
+
+
+##### Application
+
+```javascript
+
+```
+
+## Getting
+
+It's as simple as an npm:
+
+```
+npm install newnew
+``
--- test/newnew-test.js
+++ test/newnew-test.js
@@ -5,20 +5,17 @@ var a      = require('assert'),
         a.throws(function(){ newnew(ctor) }, TypeError)
     }
 
-
+var nonObject = [null, undefined, "", 1, false],
+    nonConstruct = [{}, []]
 
 suite('11.2.2 complicance: In the production NewExpression : new NewExpression')
 
 test('3. If Type(constructor) is not Object, throw a TypeError exception', function(){
-    var badTypes        = [null, "", 1, false]
-
-    badTypes.forEach(throwsTypeError)
+    nonObject.forEach(throwsTypeError)
 })
 
 test('4. If constructor does not implement the [[Construct]] internal method, throw a TypeError exception.', function(){
-    var noConstruct = [{}, []]
-
-    noConstruct.forEach(throwsTypeError)
+    nonConstruct.forEach(throwsTypeError)
 })
 
 
@@ -26,10 +23,10 @@ test('5. Return[s the same] result [as] calling the [[Construct]] internal metho
     
     var ctors = [
         function(){ this.testProp = 'test value' },
-        function(){ return {} },
-        function(){ return [] },
-        function(){ return null },
-        function(){ return true }
+        function(){ this.testProp = 'test value'; return {} },
+        function(){ this.testProp = 'test value'; return [] },
+        function(){ this.testProp = 'test value'; return null },
+        function(){ this.testProp = 'test value'; return true }
     ]
 
     ctors.forEach(function(Ctor){ Ctor.prototype = {} })
@@ -47,15 +44,23 @@ test('5. Return[s the same] result [as] calling the [[Construct]] internal metho
 
 suite('11.2.2 complicance: In the production MemberExpression : new MemberExpression Arguments')
 
+test('4. If Type(constructor) is not Object, throw a TypeError exception.', function(){
+    nonObject.forEach(function(Ctor){
+        a.throws(function(){ newnew(Ctor, 'test', 'args') }, TypeError)
+    })
+})
 
+test('5. If constructor does not implement the [[Construct]] internal method, throw a TypeError exception.', function(){
+    nonConstruct.forEach(function(Ctor){
+        a.throws(function(){ newnew(Ctor, 'test', 'args') }, TypeError)
+    })
+})
 
+test('6. Return the result of calling the [[Construct]] internal method on  constructor, providing the list argList as the argument values', function(){
+    var Ctor = function(a, b){ this.a = a; this.b = b }
 
-
-
-
-
-
-
+    a.deepEqual(new Ctor(1, 2), newnew(Ctor, 1, 2))
+})
 
 
 suite('generic tests')
