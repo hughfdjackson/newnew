785dd2d8108f383d0b498c6abf48c40b8530bd70
hugh fd jackson <hughfdjackson@googlemail.com>
2012-10-30 04:57:05 -0400

    fleshing out support for the first production (no args) and [[Construct]]

--- src/newnew.js
+++ src/newnew.js
@@ -1,10 +1,9 @@
 
 module.exports = function(Ctor){
-    var args = [].slice.call(arguments, 1)
+    var args = [].slice.call(arguments, 1),
+        o    = Object.create(Ctor.prototype)
 
-    var o = Object.create(Ctor.prototype)
+    var ret = Ctor.apply(o, args)
 
-    Ctor.apply(o, args)
-
-    return o
+    return typeof ret == 'object' && ret !== null ? ret : o
 }
--- test/newnew-test.js
+++ test/newnew-test.js
@@ -1,8 +1,65 @@
 var a      = require('assert'),
-    newnew = require('../')
+    newnew = require('../'),
+    sinon  = require('sinon')
+    throwsTypeError = function(ctor){
+        a.throws(function(){ newnew(ctor) }, TypeError)
+    }
 
 
-suite('newnew')
+
+suite('11.2.2 complicance: In the production NewExpression : new NewExpression')
+
+test('3. If Type(constructor) is not Object, throw a TypeError exception', function(){
+    var badTypes        = [null, "", 1, false]
+
+    badTypes.forEach(throwsTypeError)
+})
+
+test('4. If constructor does not implement the [[Construct]] internal method, throw a TypeError exception.', function(){
+    var noConstruct = [{}, []]
+
+    noConstruct.forEach(throwsTypeError)
+})
+
+
+test('5. Return[s the same] result [as] calling the [[Construct]] internal method on constructor, providing no arguments (that is, an empty list of arguments).', function(){
+    
+    var ctors = [
+        function(){ this.testProp = 'test value' },
+        function(){ return {} },
+        function(){ return [] },
+        function(){ return null },
+        function(){ return true }
+    ]
+
+    ctors.forEach(function(Ctor){ Ctor.prototype = {} })
+
+    ctors.forEach(function(Ctor){
+        var o  = newnew(Ctor),
+            o2 = new Ctor
+
+        a.deepEqual(o, o2)
+        a.equal(o instanceof Ctor, o2 instanceof Ctor)
+        a.equal(Object.getPrototypeOf(o), Object.getPrototypeOf(o2))
+    })
+})
+
+
+suite('11.2.2 complicance: In the production MemberExpression : new MemberExpression Arguments')
+
+
+
+
+
+
+
+
+
+
+
+
+suite('generic tests')
+
 
 test('it behaves like new on a custom-made constructor', function(){
     var Ctor  = function(){
